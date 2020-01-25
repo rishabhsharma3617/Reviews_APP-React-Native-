2f645cc4b2652551875c15cf356266a421feb252
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-25 18:56:46 +0530

    final edit the about

--- myProject/screens/about.js
+++ myProject/screens/about.js
@@ -5,7 +5,9 @@ import { globalStyles } from '../styles/global'
 export default function About() {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>This is about</Text>
+            <Text style={globalStyles.titleText}>
+                Hey , I am Rishabh Sharma ,I am a traveller , food-lover , technology enthusiast and have a keen interest in human psychology and i like to buld apps also . HAppy COding :
+            </Text>
         </View>
     )
 }
