9d463a69e19b869611585346ef062f257d0c03d0
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-01-25 14:47:54 +0530

    added the card

---
+++ myProject/shared/card.js
@@ -0,0 +1,7 @@
+import React , {Component} from 'react'
+import { StyleSheet , Text , View} from 'react-native'
+import { CardAnimationContext } from 'react-navigation-stack'
+
+export default function Card() {
+
+}
